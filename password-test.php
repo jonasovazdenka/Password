b3da7c71cc670114a49bb11d23661e28ab0bcd9c
Žofkaa <86010709+jonasovazdenka@users.noreply.github.com>
2023-02-12 21:21:25 +0100

    Update password-test.php

--- password-test.php
+++ password-test.php
@@ -1,18 +1,38 @@
 <?php
+include "password.php";
 
-class PasswordTest {
-    public function testGenerate() {
-        $password = Password::generate(10, true);
-        if (strlen($password) != 10) {
-            throw new Exception("Password length is incorrect");
+class PasswordTest
+{
+    public function testGenerate()
+    {
+        $password = new Password("");
+        $generatedPassword = $password->generate(10, "strong");
+
+        if (strlen($generatedPassword) != 10) {
+            echo "Test generate() failed: wrong password lenght\n";
+        } else {
+            echo "Test generate() done\n";
+            echo "Generated Password: " . $generatedPassword . "\n";
         }
     }
 
-    public function testCheck() {
-        $password = new Password('password123');
-        $password2 = 'password123';
-        if ($password->check($password2) !== true) {
-            throw new Exception("Passwords do not match");
+    public function testCheck()
+    {
+        $password = new Password("password");
+        $randomPassword = $password->generate();
+
+        if (!$password->check("password")) {
+            echo "Test check() failed: Bad password verification\n";
+        } elseif ($password->check($randomPassword)) {
+            echo "Test check() failed: Bad password verification \n";
+        } else {
+            echo "Test check() done\n";
+            echo "Passwords do not match.\n";
+            echo "Password compared: " . $randomPassword . "\n";
         }
     }
 }
+
+$passwordTest = new PasswordTest();
+$passwordTest->testGenerate();
+$passwordTest->testCheck();
